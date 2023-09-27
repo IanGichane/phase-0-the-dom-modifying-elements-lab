cc39694c5a7f2484c3705574d049bdceaaced947
Ian <gichaneian@gmail.com>
2023-09-27 22:27:27 +0300

    Modifying the DOM assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+/**
+ * const element = document.createElement('div')
+document.body.append(element)
+
+
+
+const ul = document.createElement('ul')
+for(let i=0;i<3;i++){
+    const li = document.createElement('li');
+    li.textContent= (i+1).toString();
+    ul.append(li);
+}
+element.append(ul)
+ul.remove()
+ */
+
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.id='victory'
+newHeader.textContent='YOUR-NAME is the champion'
+document.body.append(newHeader)
